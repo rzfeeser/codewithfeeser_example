7e4067e31264b3eb7f1b00720ef4d4b57093a8cb
RZFeeser <rzfeeser@users.noreply.github.com>
2023-11-23 02:06:36 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""RZFeeser | @CodeWithFeeser
+   https://youtube.com/@CodeWithFeeser
+   
+   A simple python script"""
+
+def main():
+  print("hello codewithfeeser students!")
+
+if __name__ == "__main__":
+  main()
